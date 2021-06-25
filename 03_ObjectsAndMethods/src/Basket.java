a909dd92e6b088225f4d03240284bcd98aca3e16
Dmitrii Abdiev <abdiev.dn@yandex.by>
2021-06-25 03:06:53 +0300

    variable totalWeight added methods with weight counting added Main modified for compile test Task 1 completed

--- 03_ObjectsAndMethods/src/Basket.java
+++ 03_ObjectsAndMethods/src/Basket.java
@@ -4,6 +4,7 @@ public class Basket {
     private String items = "";
     private int totalPrice = 0;
     private int limit;
+    private double totalWeight = 0;
 
     public Basket() {
         increaseCount(1);
@@ -34,6 +35,16 @@ public class Basket {
         add(name, price, 1);
     }
 
+    public void add(String name, int price, double weight) {
+        add(name, price, 1);
+        totalWeight += weight;
+    }
+
+    public void add(String name, int price, int count, double weight) {
+        add(name, price, count);
+        totalWeight += weight * count;
+    }
+
     public void add(String name, int price, int count) {
         boolean error = false;
         if (contains(name)) {
@@ -61,6 +72,10 @@ public class Basket {
 
     public int getTotalPrice() {
         return totalPrice;
+    }
+
+    public double getTotalWeight() {
+        return totalWeight;
     }
 
     public boolean contains(String name) {
--- 03_ObjectsAndMethods/src/Main.java
+++ 03_ObjectsAndMethods/src/Main.java
@@ -4,5 +4,24 @@ public class Main {
         Basket basket = new Basket();
         basket.add("Milk", 40);
         basket.print("Milk");
+        System.out.println("Total price: " + basket.getTotalPrice());
+        System.out.println("Total weight: " + basket.getTotalWeight());
+        System.out.println();
+
+        Basket basket1 = new Basket();
+        basket1.add("Beer", 80, 500.00);
+        basket1.add("Chips", 60, 300.00);
+        basket1.print("Basket #1");
+        System.out.println("Total price: " + basket1.getTotalPrice());
+        System.out.println("Total weight: " + basket1.getTotalWeight());
+        System.out.println();
+
+        Basket basket2 = new Basket();
+        basket2.add("Pen", 10, 5, 20.00);
+        basket2.add("Glue", 25, 2, 40.00);
+        basket2.print("Basket #2");
+        System.out.println("Total price: " + basket2.getTotalPrice());
+        System.out.println("Total weight: " + basket2.getTotalWeight());
+        System.out.println();
     }
 }
