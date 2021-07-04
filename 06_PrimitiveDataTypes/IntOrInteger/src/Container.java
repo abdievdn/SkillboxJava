f41da8860e08fac7194cccafd790f69179eed15c
Dmitrii Abdiev <abdiev.dn@yandex.by>
2021-07-05 01:12:16 +0300

    Container class fixed error with null initialization Main class is supplemented with the method charsCodes Task 6.9 completed

--- 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
@@ -1,5 +1,9 @@
 public class Container {
-    private Integer count;
+    private Integer count; // count, ссылка на объект, без инициализации - null, поэтому unboxing невоможен был
+
+    public Container(Integer count) { // добавил конструктор для инициалиации count
+        this.count = count;
+    }
 
     public void addCount(int value) {
         count = count + value;
--- 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Container container = new Container();
+        Container container = new Container(0);
         container.addCount(5672);
         System.out.println(container.getCount());
 
@@ -8,5 +8,24 @@ public class Main {
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
+
+        charsCodes('А', 'Я');
+        charsCodes('Ё', 'Ё');
+        charsCodes('а', 'я');
+        charsCodes('ё', 'ё');
+        charsCodes('\uFFFF', '\uFFFF');
+
+    }
+
+    private static void charsCodes(char a, char b) {
+        for(int i = 0; i < 65536; i++) {
+            if((char) i == a) {
+                while((char) i != (char) ((int) b + 1)) {
+                    System.out.println(i + " = " + (char) i);
+                    i++;
+                }
+                return;
+            }
+        }
     }
 }
