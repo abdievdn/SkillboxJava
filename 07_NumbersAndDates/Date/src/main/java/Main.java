5687810ad5597784cceacadb722cc575d9b1ac08
Dmitrii Abdiev <abdiev.dn@yandex.by>
2021-07-09 20:27:50 +0300

    Date project completed

--- 07_NumbersAndDates/Date/src/main/java/Main.java
+++ 07_NumbersAndDates/Date/src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
 
 public class Main {
 
@@ -8,7 +12,6 @@ public class Main {
         int year = 1990;
 
         System.out.println(collectBirthdays(year, month, day));
-
     }
 
     public static String collectBirthdays(int year, int month, int day) {
@@ -16,7 +19,19 @@ public class Main {
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
-        
-        return "";
+
+        LocalDate birthday = LocalDate.of(year, month, day);
+        LocalDate today = LocalDate.now();
+        long datesDifference = birthday.until(today, ChronoUnit.YEARS);
+        DateTimeFormatter dateToString = DateTimeFormatter.ofPattern("dd.MM.yyyy - EEE", Locale.ENGLISH);
+        String result = "";
+
+        if (today.compareTo(birthday) >= 0)
+        {
+            for (int i = 0; i <= datesDifference; i++) {
+                result += i + " - " + dateToString.format(birthday.plusYears(i)) + '\n';
+            }
+        }
+        return result;
     }
 }
