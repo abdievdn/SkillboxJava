0babea7911b2b5fdd688c93f786d2a130ee91131
Dmitrii Abdiev <abdiev.dn@yandex.by>
2021-07-14 01:45:09 +0300

    EmailList class developed Main class user ui developed Homework 9.3 done

--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
@@ -1,15 +1,34 @@
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmailList {
 
+    private final TreeSet<String> emailList = new TreeSet<>();
+    private final String regex = "\\w+([\\.-]?\\w+)@\\w+([\\.-]?\\w+)\\.\\w{2,4}";
+    private final Pattern pattern = Pattern.compile(regex);
+
     public void add(String email) {
         // TODO: валидный формат email добавляется
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.find()) {
+            if (emailList.contains(email.toLowerCase())) {
+                System.out.println("Такой email уже существует в базе");
+            }
+            else {
+                emailList.add(email.toLowerCase());
+                System.out.println("Email добавлен");
+            }
+        }
+        else System.out.println(Main.WRONG_EMAIL_ANSWER);
     }
 
     public List<String> getSortedEmails() {
         // TODO: возвращается список электронных адресов в алфавитном порядке
-        return Collections.emptyList();
+        ArrayList<String> sortedEmails = new ArrayList<>();
+        for (String email : emailList)
+            sortedEmails.add(email);
+        return sortedEmails;
     }
 
 }
--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
@@ -19,15 +21,36 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        final String commandGroup = "command";
+        final String textGroup = "text";
+        final String addCommand = "ADD";
+        final String listCommand = "LIST";
+        final String regex = String.format("(?<%s>%s|%s)(\\s(?<%s>.*))?", commandGroup, addCommand, listCommand, textGroup);
+        final Pattern pattern = Pattern.compile(regex);
+        EmailList emailList = new EmailList();
+
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("0")) {
                 break;
             }
-            
-            //TODO: write code here
-            
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.find()) {
+                switch (matcher.group(commandGroup)) {
+                    case addCommand:
+                        if (matcher.group(textGroup) != null)
+                            emailList.add(matcher.group(textGroup));
+                        break;
+                    case listCommand:
+                        System.out.println("LIST");
+                        for (String email : emailList.getSortedEmails())
+                            System.out.println(email);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
