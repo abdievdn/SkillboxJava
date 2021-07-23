4b44930d63abd5a5ffa672f9dc62350c1d41d20f
Dmitrii Abdiev <abdiev.dn@yandex.by>
2021-07-24 02:27:50 +0300

    sortBySalaryAndAlphabet method developed Homework 11.1 completed

--- 11_AdvancedOOPFeatures/homework_11.1/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.1/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -7,9 +9,16 @@ public class Main {
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
         System.out.println(staff);
+        System.out.println();
+        sortBySalaryAndAlphabet(staff);
     }
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
-        //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
+        ArrayList<Employee> sortedStaff = new ArrayList<>(staff);
+        Collections.sort(staff, ((o1, o2) -> o1.getName().compareTo(o2.getName())));
+        Collections.sort(staff, ((o1, o2) -> o1.getSalary().compareTo(o2.getSalary())));
+        for (Employee employee : staff) {
+            System.out.println(employee);
+        }
     }
 }
