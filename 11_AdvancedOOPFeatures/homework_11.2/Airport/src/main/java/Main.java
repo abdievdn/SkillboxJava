52a1d38e85e3940db881f71d37cce4b26d910208
Dmitrii Abdiev <abdiev.dn@yandex.by>
2021-07-25 03:15:15 +0300

    findPlanesLeavingInTheNextTwoHours realised

--- 11_AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
@@ -3,6 +3,7 @@ import com.skillbox.airport.Flight;
 import com.skillbox.airport.Terminal;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,26 +12,23 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
 
-
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        List<Flight> nextTwoHoursFlights = new ArrayList<>();
+        List<Flight> nextTwoHoursFlights;
         Date time = new Date();
-        long departureTime = 1000 * 60 * 60 * 2;
+        int departureTime = time.getDate() + (1000 * 60 * 60 * 2);
 
-        airport.getTerminals()
+        nextTwoHoursFlights = airport.getTerminals()
                 .stream()
                 .map(Terminal::getFlights)
+                .flatMap(Collection::stream)
                 .filter(flight -> flight.getType() == Flight.Type.DEPARTURE &&
                       (flight.getDate().compareTo(time) >= 0 && flight.getDate().compareTo(time) <= departureTime))
-                .reduce(t -> nextTwoHoursFlights.add(t));
+                .collect(Collectors.toList());
 
         return nextTwoHoursFlights;
     }
 
 }
-
-//.filter(flight -> flight.getType() == Flight.Type.DEPARTURE &&
-//(flight.getDate().compareTo(time) >= 0 && flight.getDate().compareTo(time) <= departureTime))
