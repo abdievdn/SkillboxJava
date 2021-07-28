60b6acf6df59c2a5b0c18ed094c58a3070d1a8f0
Dmitrii Abdiev <abdiev.dn@yandex.by>
2021-07-28 23:02:02 +0300

    Exceptions realised ic CustomerStorage class Homework 12.1 done

--- 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,7 +1,10 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomerStorage {
+    public static final String WRONG_FORMAT = " wrong format!";
     private final Map<String, Customer> storage;
 
     public CustomerStorage() {
@@ -13,10 +16,23 @@ public class CustomerStorage {
         final int INDEX_SURNAME = 1;
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
+        final Pattern PHONE_PATTERN = Pattern.compile("\\+\\d{11}");
+        final Pattern EMAIL_PATTERN = Pattern.compile("\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*\\.\\w{2,4}");
 
         String[] components = data.split("\\s+");
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Command input" + WRONG_FORMAT);
+        }
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+        if (!EMAIL_PATTERN.matcher(components[2]).find()) {
+            throw new IllegalArgumentException("Email input " + WRONG_FORMAT);
+        }
+        if (!PHONE_PATTERN.matcher(components[3]).find()) {
+            throw new IllegalArgumentException("Phone input" + WRONG_FORMAT);
+        }
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+
+
     }
 
     public void listCustomers() {
