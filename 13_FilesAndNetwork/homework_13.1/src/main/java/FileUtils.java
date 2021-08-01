61a6cbbd3709130965fb3085e65dec1a00aa6266
Dmitrii Abdiev <abdiev.dn@yandex.by>
2021-08-01 13:44:40 +0300

    FileUtils method realised Main class realised

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -1,6 +1,25 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        long size = 0;
+        try {
+            size = Files.walk(Paths.get(path))
+                    .filter(Files::isRegularFile)
+                    .mapToLong(f -> {
+                        try {
+                            return Files.size(f);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        return 0;
+                    })
+                    .sum();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return size;
     }
 }
--- 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        for (; ; ) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Введите путь к директории: ");
+            String path = input.nextLine();
+            System.out.println("Размер папки " + path + " cоставляет " + printSize(FileUtils.calculateFolderSize(path)) + '\n');
+        }
+    }
 
+    private static String printSize(Long size) {
+        if (size <= 0) return "0";
+        String[] units = {"б", "Кб", "Мб", "Гб"};
+        int unitsGroup = (int) (Math.log10(size)/Math.log10(1024));
+        return String.format("%.1f %s", (size/Math.pow(1024, unitsGroup)), units[unitsGroup]);
     }
 }
