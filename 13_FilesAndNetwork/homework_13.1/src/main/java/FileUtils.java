2c77139b5292759193f8123b5921dbe430562c06
Dmitrii Abdiev <abdiev.dn@yandex.by>
2021-08-03 00:19:45 +0300

    FileUtils method is redesigned using recursion

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -1,4 +1,4 @@
-import java.nio.file.Files;
+import java.io.File;
 import java.nio.file.Paths;
 
 public class FileUtils {
@@ -6,17 +6,13 @@ public class FileUtils {
     public static long calculateFolderSize(String path) {
         long size = 0;
         try {
-            size = Files.walk(Paths.get(path))
-                    .filter(Files::isRegularFile)
-                    .mapToLong(f -> {
-                        try {
-                            return Files.size(f);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        return 0;
-                    })
-                    .sum();
+            File files = new File(path);
+            if (files.isDirectory()) {
+                for (File file : files.listFiles()) {
+                    if (file.isFile()) size += file.length();
+                    else size += calculateFolderSize(file.getPath());
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
