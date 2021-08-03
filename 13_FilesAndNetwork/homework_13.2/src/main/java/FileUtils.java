2686984af377c392a7fe2346fb1630a8d8ef045f
Dmitrii Abdiev <abdiev.dn@yandex.by>
2021-08-03 03:43:33 +0300

    copyFolder method created

--- 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
@@ -1,5 +1,25 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
+    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
         // TODO: write code copy content of sourceDirectory to destinationDirectory
+        Path source = Paths.get(sourceDirectory);
+        Path dest = Paths.get(destinationDirectory);
+        try {
+            if (!Files.exists(dest))
+                Files.createDirectory(dest);
+            for (Path f : Files.newDirectoryStream(source)) {
+                if (Files.isDirectory(f))
+                    copyFolder(f.toString(), dest.resolve(f.getFileName()).toString());
+                if (!Files.exists(dest.resolve(f.getFileName())))
+                    Files.copy(f, dest.resolve(f.getFileName()));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Copy files.");
+            System.out.print("Input source directory: ");
+            String source = input.nextLine();
+            System.out.print("Input destination directory: ");
+            String dest = input.nextLine();
+            FileUtils.copyFolder(source, dest);
+            System.out.println("Completed!");
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
